231c712765da048ed6016f1b740c012b3d859008
root <Bryan Dang>
2018-09-05 18:33:54 +0000

    adding helloword

---
+++ Helloworld.cpp
@@ -0,0 +1,25 @@
+
+/*
+Bryan Dang
+2295344
+bdang@chapman.edu
+CPSC 350 Data Structor and Algorithms
+Assignment 0
+
+A C++ code that prints out "Hello World"
+
+*/
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Hello World" << endl;
+
+  system("pause");
+  return 0;
+}
